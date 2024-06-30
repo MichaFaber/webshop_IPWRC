d2c0cb43e0a7833148bac897d61afc4434fe75fb
Micha Faber <micha_faber@live.nl>
2024-06-30 19:55:35 +0200

    update register

--- Webshop_backend/server.js
+++ Webshop_backend/server.js
@@ -49,9 +49,10 @@ app.post('/api/register', async (req, res) => {
   }
 
   const hashedPassword = await bcrypt.hash(password, 10);
+  console.log(hashedPassword)
   try {
     db.query(
-      'INSERT INTO users (username, password, email, role) VALUES (?, ?, ?, ?) ',
+      'INSERT INTO users (username, password, email, role) VALUES (?, ?, ?, ?);',
       [username, hashedPassword, email, role],
       (result, error)  => {
         console.log(result, error)
